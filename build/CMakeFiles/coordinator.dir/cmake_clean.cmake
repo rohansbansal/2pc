2aeb91938e6d3fe2e75d8cf6dd17c23ce7699d01
Rohan Bansal <rsb344@cornell.edu>
2023-07-30 12:29:56 -0700

    first commit

---
+++ build/CMakeFiles/coordinator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coordinator.dir/coordinator/main.cc.o"
+  "CMakeFiles/coordinator.dir/coordinator/main.cc.o.d"
+  "coordinator"
+  "coordinator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coordinator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
